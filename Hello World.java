ad5f924a7470ac552b2d26325e1b0a2b7c211f0d
swiftsync <swiftsynctest@gmail.com>
2016-11-28 16:52:06 +0530

    Maine Changes
    FInal Commit date 28-11-2016

--- Hello World.java
+++ Hello World.java
@@ -4,10 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello World1223sr!");
-=======
-		System.out.println("Hello World123sr!");
->>>>>>> origin/SwiftSync
 	}
 }
