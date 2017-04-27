1fa0e19350f9667537429d3580774c5d3bab420a
Niket Patil <npatil@digite.in>
2017-04-27 13:42:44 +0530

    IssueId::DEF12 Reviewer::admin Status::Done Comments:: Committing changes.

--- Hello World.java
+++ Hello World.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World End of Year! 2017");
+		System.out.println("Hello World End of Year 2017!");
 	}
 }
